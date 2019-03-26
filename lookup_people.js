db20af28519b63d05747f903b802e46ff653aad7
karen1au <kagemikaka@hotmail.com>
2019-03-26 17:54:03 +0000

    modified functions into knex

--- lookup_people.js
+++ lookup_people.js
@@ -1,14 +1,17 @@
-const dh = require('./test_script.js');
+const dh = require('./knex_script.js');
 
 const name = process.argv[2];
 
 dh.getPeopleByName(name, function(err, results){
-    if (err) { console.log('error happen');}
-    console.log(results.rows);
+    if (err) { 
+        console.log('error happen',err);
+        return;
+    }
+    // console.log(results);
     console.log('Searching ...');
-    console.log(`Found ${results.rows.length} person(s) by the name '${name}':`);
-    results.rows.forEach(row => {
-        console.log(`- ${results.rows.indexOf(row)+1}: ${row.first_name} ${row.last_name}, born '${row.birthdate}'`);
+    console.log(`Found ${results.length} person(s) by the name '${name}':`);
+    results.forEach((row, index) => {
+        console.log(`- ${index+1}: ${row.first_name} ${row.last_name}, born '${row.birthdate}'`);
     });
     dh.close();
 });
